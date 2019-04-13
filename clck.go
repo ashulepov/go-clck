14567d5357510f953452fe6fdbb74bbb679f622b
Andrey Shulepov <andrey.shulepov@gmail.com>
2019-04-13 18:54:56 +0500

    escape long url

--- clck.go
+++ clck.go
@@ -3,14 +3,15 @@ package clck
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 // ShortURL returns short URL from clck.ru service
-func ShortURL(url string) (string, error) {
+func ShortURL(longURL string) (string, error) {
 	client := &http.Client{}
-	resp, err := client.Get(`https://clck.ru/--?url=` + url)
+	resp, err := client.Get(`https://clck.ru/--?url=` + url.QueryEscape(longURL))
 	if err != nil {
-		return url, err
+		return longURL, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
